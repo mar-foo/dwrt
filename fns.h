f680d00f68750e3e94d1a894a9c56bf20568f5bc
Mario Forzanini <mario.forzanini@studenti.unimi.it>
2022-06-27 19:32:11 +0200

    Add copyright notice

--- fns.h
+++ fns.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright ©️ 2022 Mario Forzanini <mf@marioforzanini.com>
+ *
+ * This file is part of derive.
+ *
+ * Derive is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Derive is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with derive. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 Symbol*	func_alloc(char*);
 Symbol*	lparen_alloc();
 Symbol*	rparen_alloc();
