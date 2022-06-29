b7126d48861b3a1f16dbc6fbe10272d4cd2688bb
Mario Forzanini <mario.forzanini@studenti.unimi.it>
2022-06-29 12:07:00 +0200

    Add parser tests

--- test/test_parse.c
+++ test/test_parse.c
@@ -220,16 +220,112 @@ START_TEST(test_lex_full)
 }
 END_TEST
 
+START_TEST(test_parse_empty)
+{
+	Parser *p;
+
+	p = p_init("files/test_parse_empty.txt");
+
+	ck_assert(parse(p) == 0);
+	ck_assert_ptr_null(p->ast);
+	p_cleanup(p);
+}
+END_TEST
+
+START_TEST(test_parse_malformed_expression)
+{
+	Parser *p;
+	p = p_init("files/test_parse_malformed_expression.txt");
+
+	ck_assert_msg(parse(p) < 0, "Parser should fail but it doesn't");
+	ck_assert_str_eq(p->err, "files/test_parse_malformed_expression.txt:1 malformed expression\n");
+	p_cleanup(p);
+}
+END_TEST
+
+START_TEST(test_parse_non_parenthesized)
+{
+	Parser *p;
+	p = p_init("files/test_parse_non_parenthesized.txt");
+	ck_assert_msg(parse(p) == 0, p->err);
+
+	ck_assert(p->ast->sym->content->op == '/');
+	ck_assert_str_eq(p->ast->right->sym->content->func, "sin");
+	ck_assert(num_equal(p->ast->right->right->sym, 2));
+	ck_assert_ptr_null(p->ast->right->left);
+	ck_assert(p->ast->left->sym->content->op == '+');
+	ck_assert(num_equal(p->ast->left->right->sym, 4));
+	ck_assert(p->ast->left->left->sym->content->op == '*');
+	ck_assert(p->ast->left->left->right->sym->content->var == 'x');
+	ck_assert(num_equal(p->ast->left->left->left->sym, 2));
+
+	p_cleanup(p);
+}
+END_TEST
+
+START_TEST(test_parse_parenthesized)
+{
+	Parser *p;
+
+	p = p_init("files/test_parse_parenthesized.txt");
+	ck_assert_msg(parse(p) == 0, p->err);
+
+	ck_assert(p->ast->sym->content->op == '/');
+	ck_assert_str_eq(p->ast->right->sym->content->func, "sin");
+	ck_assert(num_equal(p->ast->right->right->sym, 2));
+	ck_assert_ptr_null(p->ast->right->left);
+	ck_assert(p->ast->left->sym->content->op == '+');
+	ck_assert(num_equal(p->ast->left->right->sym, 4));
+	ck_assert(p->ast->left->left->sym->content->op == '*');
+	ck_assert(p->ast->left->left->right->sym->content->var == 'x');
+	ck_assert(num_equal(p->ast->left->left->left->sym, 2));
+
+	p_cleanup(p);
+}
+END_TEST
+
+START_TEST(test_parse_unbalanced_left_parenthesis)
+{
+	Parser *p;
+	p = p_init("files/test_parse_unbalanced_left_parenthesis.txt");
+
+	ck_assert_msg(parse(p) < 0, "Parser should fail but it doesn't");
+	ck_assert_str_eq(p->err, "files/test_parse_unbalanced_left_parenthesis.txt:1 unbalanced parenthesis\n");
+	p_cleanup(p);
+}
+END_TEST
+
+START_TEST(test_parse_unbalanced_right_parenthesis)
+{
+	Parser *p;
+	p = p_init("files/test_parse_unbalanced_right_parenthesis.txt");
+
+	ck_assert_msg(parse(p) < 0, "Parser should fail but it doesn't");
+	ck_assert_str_eq(p->err, "files/test_parse_unbalanced_right_parenthesis.txt:1 unbalanced parenthesis\n");
+	p_cleanup(p);
+}
+END_TEST
+
+/* START_TEST(test_parse_function_application) */
+/* { */
+/* } */
+/* END_TEST */
+
+/* START_TEST(test_parse_function_no_parenthesis) */
+/* { */
+/* } */
+/* END_TEST */
 
 Suite*
 parse_suite(void)
 {
 	Suite *s;
-	TCase *tc_lex;
+	TCase *tc_lex, *tc_parse;
 
 	s = suite_create("parse");
 
 	tc_lex = tcase_create("lex");
+	tc_parse = tcase_create("parse");
 
 	tcase_add_test(tc_lex, test_l_init_non_exist);
 	tcase_add_test(tc_lex, test_l_init_exist);
@@ -240,7 +336,15 @@ parse_suite(void)
 	tcase_add_test(tc_lex, test_lex_rparen);
 	tcase_add_test(tc_lex, test_lex_symbol);
 
+	tcase_add_test(tc_parse, test_parse_empty);
+	tcase_add_test(tc_parse, test_parse_malformed_expression);
+	tcase_add_test(tc_parse, test_parse_non_parenthesized);
+	tcase_add_test(tc_parse, test_parse_parenthesized);
+	tcase_add_test(tc_parse, test_parse_unbalanced_left_parenthesis);
+	tcase_add_test(tc_parse, test_parse_unbalanced_right_parenthesis);
+
 	suite_add_tcase(s, tc_lex);
+	suite_add_tcase(s, tc_parse);
 
 	return s;
 }
