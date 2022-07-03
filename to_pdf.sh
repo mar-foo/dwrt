9aa6fdf16e21bfde0977ea8cbb1287d016a898eb
Mario Forzanini <mario.forzanini@studenti.unimi.it>
2022-07-03 19:55:02 +0200

    Add script to display latex pdf

---
+++ to_pdf.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+if test -z $1; then
+	echo "usage: $0 variable" >&2
+	exit 1
+fi
+
+read -r func
+
+cat <<EOF | pdflatex >/dev/null
+\documentclass{standalone}
+\begin{document}
+\$$(echo "$func" | ./derive "$1")\$
+\end{document}
+EOF
+
+zathura texput.pdf ; rm -f texput.*
