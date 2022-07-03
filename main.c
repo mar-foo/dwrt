d7652828e416a1030b85dab9ab573f68ce5362bb
Mario Forzanini <mario.forzanini@studenti.unimi.it>
2022-07-03 21:04:50 +0200

    s/Derive/Dwrt/g

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
  *
  * This file is part of dwrt.
  *
- * Derive is free software: you can redistribute it and/or modify it
+ * Dwrt is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * Derive is distributed in the hope that it will be useful, but WITHOUT
+ * Dwrt is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
